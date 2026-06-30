eaef68a0061f5ba4339b608625c1f8f68bf3bad9
LeanAranda <arandaleandro2003@gmail.com>
2026-06-30 15:41:04 -0300

    db admin test credentials

---
+++ migrations/0002_seed_admin.sql
@@ -0,0 +1,5 @@
+-- Seed the default admin user
+-- Username: admin
+-- Password: admin123 / adminBarMenu
+INSERT INTO admin_users (username, password_hash)
+VALUES ('admin', '$2b$10$zjoq7mfDI0CDrahiUceLyO6YbIxtuIYZal9H8iMyQK8ZG9Es9UR9e');
